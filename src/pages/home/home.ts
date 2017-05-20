7f9b47c7d5503dc7d2be9a97193d15987bcc5d9e
Aleix Morgadas <aleix.morgadas@gmail.com>
2017-05-20 21:33:09 +0200

    CHANGED home.hml displays the icon next to NEM Authenticator header CHANGED the specified the domain "bob.nem.ninja" CHANGED home.ts, now pulls the incoming unconfirmed transactions every 5 seconds

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,22 +1,28 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component, OnInit} from "@angular/core";
+import {NavController} from "ionic-angular";
 import {UnconfirmedTransactions} from "nem-library/dist/src/models/UnconfirmedTransactions";
 import {AccountHttp} from "nem-library";
+import {Observable} from "@reactivex/rxjs";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
-
+export class HomePage implements OnInit {
   unconfirmedTransactions: UnconfirmedTransactions;
+  private unconfirmedTransactionsEndpoint: Observable<any>;
 
   constructor(public navCtrl: NavController) {
-    const accountHttp = new AccountHttp();
-    accountHttp.unconfirmedTransactions('TAZM4LSMAHPDEKHJJO6MVMBQ3C2KCJME5A2DYFOJ')
-      .subscribe(
-        value => this.unconfirmedTransactions = value
-      )
+    const accountHttp = new AccountHttp({domain: 'bob.nem.ninja'});
+    this.unconfirmedTransactionsEndpoint = Observable.interval(5000).flatMap(x => {
+      return accountHttp.unconfirmedTransactions('TAZM4LSMAHPDEKHJJO6MVMBQ3C2KCJME5A2DYFOJ');
+    });
+  }
+
+  ngOnInit(): void {
+    this.unconfirmedTransactionsEndpoint.subscribe(
+      value => this.unconfirmedTransactions = value
+    );
   }
 
 }
