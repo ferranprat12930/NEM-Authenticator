981550f42e0cf2afdd423c2e3362e9c42a11af14
Aleix Morgadas <aleix.morgadas@gmail.com>
2017-05-21 20:10:22 +0200

    UPDATED home.ts with the cosignTransaction action DOES NOT WORK YET

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from "@angular/core";
 import {NavController} from "ionic-angular";
-import {UnconfirmedTransactions} from "nem-library/dist/src/models/UnconfirmedTransactions";
-import {AccountHttp} from "nem-library";
+import {UnconfirmedTransaction, UnconfirmedTransactions} from "nem-library/dist/src/models/UnconfirmedTransactions";
+import {AccountHttp, TransactionHttp} from "nem-library";
 import {Observable} from "rxjs";
 
 @Component({
@@ -23,6 +23,27 @@ export class HomePage implements OnInit {
     this.unconfirmedTransactionsEndpoint.subscribe(
       value => this.unconfirmedTransactions = value
     );
+  }
+
+  cosignTransaction(unconfirmedTransaction: UnconfirmedTransaction) {
+    const transactionHttp = new TransactionHttp();
+    transactionHttp.prepareAnnounce({
+      privateKey: "",
+      transaction: {
+        timeStamp: unconfirmedTransaction.transaction.timeStamp,
+        fee: 6000000,
+        type: 4098,
+        deadline: unconfirmedTransaction.transaction.deadline,
+        version: unconfirmedTransaction.transaction.version,
+        signer: "5d5d829644625eb6554273f70b1187d904761fab4c5c0e5f01666f6725e9278b",
+        otherHash: {
+          data: unconfirmedTransaction.meta.data
+        },
+        otherAccount: "TAZM4LSMAHPDEKHJJO6MVMBQ3C2KCJME5A2DYFOJ"
+      }
+    }).subscribe(x => {
+      console.log("SUCCESS", x)
+    }, err => console.log("ERROR", err))
   }
 
 }
