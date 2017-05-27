d25186049c37a487e8da0fb1bff7fec0b4597ccf
Aleix Morgadas <aleix.morgadas@gmail.com>
2017-05-27 12:04:22 +0200

    CHANGED home.ts showing loader until pending transactions are fetched

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from "@angular/core";
-import {NavController} from "ionic-angular";
+import {Loading, LoadingController, NavController} from "ionic-angular";
 import {UnconfirmedTransaction, UnconfirmedTransactions} from "nem-library/dist/src/models/UnconfirmedTransactions";
 import {AccountHttp, TransactionHttp} from "nem-library";
 import {Observable} from "rxjs";
@@ -11,8 +11,14 @@ import {Observable} from "rxjs";
 export class HomePage implements OnInit {
   unconfirmedTransactions: UnconfirmedTransactions;
   private unconfirmedTransactionsEndpoint: Observable<any>;
+  loader: Loading;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+              private loadingCtrl: LoadingController) {
+    this.loader = loadingCtrl.create({
+      content: "Please wait..."
+    });
+    this.loader.present();
     const accountHttp = new AccountHttp({domain: 'bob.nem.ninja'});
     this.unconfirmedTransactionsEndpoint = Observable.interval(5000).flatMap(x => {
       return accountHttp.unconfirmedTransactions('TAZM4LSMAHPDEKHJJO6MVMBQ3C2KCJME5A2DYFOJ');
@@ -21,29 +27,16 @@ export class HomePage implements OnInit {
 
   ngOnInit(): void {
     this.unconfirmedTransactionsEndpoint.subscribe(
-      value => this.unconfirmedTransactions = value
+      value => {
+        this.unconfirmedTransactions = value;
+        this.loader.dismiss();
+      }
     );
   }
 
   cosignTransaction(unconfirmedTransaction: UnconfirmedTransaction) {
-    const transactionHttp = new TransactionHttp();
-    transactionHttp.prepareAnnounce({
-      privateKey: "",
-      transaction: {
-        timeStamp: unconfirmedTransaction.transaction.timeStamp,
-        fee: 6000000,
-        type: 4098,
-        deadline: unconfirmedTransaction.transaction.deadline,
-        version: unconfirmedTransaction.transaction.version,
-        signer: "5d5d829644625eb6554273f70b1187d904761fab4c5c0e5f01666f6725e9278b",
-        otherHash: {
-          data: unconfirmedTransaction.meta.data
-        },
-        otherAccount: "TAZM4LSMAHPDEKHJJO6MVMBQ3C2KCJME5A2DYFOJ"
-      }
-    }).subscribe(x => {
-      console.log("SUCCESS", x)
-    }, err => console.log("ERROR", err))
+    //const transactionHttp = new TransactionHttp();
+    
   }
 
 }
