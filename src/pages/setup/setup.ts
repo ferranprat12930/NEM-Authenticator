38e69f7849ce54ef06b1063e6c197050a952b3b0
Aleix Morgadas <aleix.morgadas@gmail.com>
2017-06-04 17:46:10 +0200

    ADDED verification steps to SetupPage

--- src/pages/setup/setup.ts
+++ src/pages/setup/setup.ts
@@ -1,5 +1,7 @@
-import {Component} from "@angular/core";
+import {Component, ViewChild} from "@angular/core";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Slides, ToastController} from "ionic-angular";
+import {Account} from "nem-library";
 
 @Component({
   selector: 'page-setup',
@@ -7,14 +9,45 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 })
 export class SetupPage {
   form: FormGroup;
+  @ViewChild(Slides) slides: Slides;
+  account: Account;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,
+              public toastCtrl: ToastController) {
     this.form = formBuilder.group({
-      privateKey: ['', Validators.compose([Validators.required, Validators.minLength(64), Validators.maxLength(66)])]
+      privateKey: ['',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(64),
+          Validators.maxLength(66)])],
+      myAccount: [false, Validators.requiredTrue]
     });
   }
 
   confirm() {
 
+  }
+
+  verifyAccount() {
+    let privateKey: string = this.form.get('privateKey').value;
+    if ((privateKey.length !== <number>64) && (privateKey.length !== <number>66)) {
+      this.toastCtrl.create({
+        message: "Check your private key",
+        duration: 1000
+      }).present();
+    } else {
+      this.account = Account.generateWithPrivateKey(privateKey);
+      this.slides.lockSwipeToNext(false);
+      this.slides.slideNext(500);
+    }
+  }
+
+  slideChanged() {
+    let currentIndex = this.slides.getActiveIndex();
+    if (currentIndex == 2 && this.account === undefined) {
+      this.slides.lockSwipeToNext(true);
+    } else {
+      this.slides.lockSwipeToNext(false);
+    }
   }
 }
