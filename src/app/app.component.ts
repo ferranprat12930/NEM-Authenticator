2063bcfc31a40a1eab3f4050ee1107b134647384
Aleix Morgadas <aleix.morgadas@gmail.com>
2017-06-04 18:29:02 +0200

    CHANGED app.component.ts check project status to load SetupPage or HomePage, showing a lodader meanwhile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,34 @@
 import {Component} from "@angular/core";
-import {Platform} from "ionic-angular";
+import {LoadingController, Platform} from "ionic-angular";
 import {StatusBar} from "@ionic-native/status-bar";
 import {SplashScreen} from "@ionic-native/splash-screen";
 import {SetupPage} from "../pages/setup/setup";
+import {Storage} from "@ionic/storage";
+import {HomePage} from "../pages/home/home";
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage: any = SetupPage;
+  rootPage: any;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform,
+              statusBar: StatusBar,
+              splashScreen: SplashScreen,
+              private storage: Storage,
+              public loadingCtrl: LoadingController) {
+    let loader = loadingCtrl.create({
+      content: "Please wait..."
+    });
+    loader.present();
+    storage.get('PRIVATE_KEY').then(privateKey => {
+      loader.dismiss();
+      if (privateKey !== null) {
+        this.rootPage = HomePage;
+      } else {
+        this.rootPage = SetupPage;
+      }
+    });
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
