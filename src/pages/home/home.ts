100cd649ba2b6d9d438899fb843e758eb58d4412
Aleix Morgadas <aleix.morgadas@gmail.com>
2017-06-04 18:31:03 +0200

    CHANGED home.ts removing hardcoded account info to get the device account

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,24 +1,17 @@
-import {Component, OnInit} from "@angular/core";
+import {Component} from "@angular/core";
 import {Loading, LoadingController, ModalController, NavController, ToastController} from "ionic-angular";
-import {
-  Account,
-  AccountHttp,
-  MultisigSignatureTransaction,
-  TransactionHttp,
-  UnconfirmedTransaction,
-  UnconfirmedTransactions
-} from "nem-library";
+import {Account, AccountHttp, MultisigSignatureTransaction, TransactionHttp, UnconfirmedTransaction} from "nem-library";
 import {Observable} from "rxjs";
 import {TransactionModal} from "./transaction.modal";
+import {Storage} from "@ionic/storage";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage implements OnInit {
-  readonly address = 'TAZM4LSMAHPDEKHJJO6MVMBQ3C2KCJME5A2DYFOJ';
+export class HomePage {
+  private account: Account;
   unconfirmedTransactions: UnconfirmedTransaction[];
-  private unconfirmedTransactionsEndpoint: Observable<UnconfirmedTransactions>;
   loader: Loading;
 
   constructor(public navCtrl: NavController,
@@ -26,25 +19,25 @@ export class HomePage implements OnInit {
               public modalCtrl: ModalController,
               public toastCtrl: ToastController,
               public accountHttp: AccountHttp,
-              public transactionHttp: TransactionHttp) {
+              public transactionHttp: TransactionHttp,
+              private storage: Storage) {
+    this.storage.get('PRIVATE_KEY').then(privateKey => {
+      this.account = Account.generateWithPrivateKey(privateKey);
+      Observable.interval(5000).startWith(0).flatMap(x => {
+        return accountHttp.unconfirmedTransactions(this.account.address);
+      }).subscribe(
+        value => {
+          this.unconfirmedTransactions = value.data.filter(x => {
+            return x.transaction.type == 4100;
+          });
+          this.loader.dismiss();
+        }
+      );
+    });
     this.loader = loadingCtrl.create({
       content: "Please wait..."
     });
     this.loader.present();
-    this.unconfirmedTransactionsEndpoint = Observable.interval(5000).startWith(0).flatMap(x => {
-      return accountHttp.unconfirmedTransactions(this.address);
-    });
-  }
-
-  ngOnInit(): void {
-    this.unconfirmedTransactionsEndpoint.subscribe(
-      value => {
-        this.unconfirmedTransactions = value.data.filter(x => {
-          return x.transaction.type == 4100;
-        });
-        this.loader.dismiss();
-      }
-    );
   }
 
   cosignTransaction(unconfirmedTransaction: UnconfirmedTransaction) {
@@ -62,18 +55,16 @@ export class HomePage implements OnInit {
   }
 
   private signTransaction(unconfirmedTransaction: UnconfirmedTransaction) {
-    const account = new Account(this.address,
-      '5d5d829644625eb6554273f70b1187d904761fab4c5c0e5f01666f6725e9278b',
-      '');
+    // TODO: Change hardcoded multisig account
     const multisigSignedTransaction = new MultisigSignatureTransaction(
       unconfirmedTransaction.transaction.timeStamp,
       unconfirmedTransaction.transaction.deadline,
       unconfirmedTransaction.transaction.fee,
       'TBUAUC3VYKPP3PJPOH7A7BCB2C4I64XZAAOZBO6N',
       {data: unconfirmedTransaction.meta.data},
-      account.publicKey
+      this.account.publicKey
     );
-    const signedTransaction = account.signTransaction(multisigSignedTransaction);
+    const signedTransaction = this.account.signTransaction(multisigSignedTransaction);
     this.transactionHttp.announceTransaction(signedTransaction).subscribe(x => {
         console.log(x);
       }
@@ -87,5 +78,4 @@ export class HomePage implements OnInit {
     });
     toast.present();
   }
-
 }
